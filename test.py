67b6ec54c7c73f37c17c6f44041d0cd2458fe2da
lizardswimmer <lizardswimmer@gmail.com>
2022-10-13 22:48:47 -0500

    use argv in test

--- test.py
+++ test.py
@@ -1,5 +1,10 @@
+import sys
+
 from Pinger import ping
 
 
 if __name__ == "__main__":
-    print(f"Pinged 1.1.1.1 in {ping('1.1.1.1')}s")
+    if len(sys.argv) > 1:
+        print(f"Pinged {sys.argv[1]} in {ping(sys.argv[1])}s")
+    else:
+        print(f"Pinged 1.1.1.1 in {ping('1.1.1.1')}s")
